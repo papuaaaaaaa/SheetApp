f32e930c1ca5d243d219519fec869f54c06eba0c
papua <nakao_shinichirou@aratana.jp>
2014-12-07 15:20:45 +0900

    set each controller

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ angular.module('App', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl: 'index-tmpl'
+                templateUrl: 'index-tmpl',
+                controller: 'SheetListController'
             })
             .when('/new', {
-                templateUrl: 'new-tmpl'
+                templateUrl: 'new-tmpl',
+                controller: 'CreationController'
             })
             .when('/sheet/:id', {
-                templateUrl: 'sheet-tmpl'
+                templateUrl: 'sheet-tmpl',
+                controller: 'SheetController'
             })
             .otherwise({
                 redirectTo: '/'
             });
-    }]);
+    }])
+    .controller('SheetListController', [function SheetListController() {/* 一覧用 */}])
+    .controller('CreationController', [function CreationController() {/* 作成用 */}])
+    .controller('SheetController', [function SheetController() {/* 詳細用 */}]);
 
