ebd263cefcb93b8952a284a733945fa895d19782
papua <nakao_shinichirou@aratana.jp>
2014-12-07 15:19:32 +0900

    routing

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 /**
  * Created by papua on 2014/12/07.
  */
+angular.module('App', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'index-tmpl'
+            })
+            .when('/new', {
+                templateUrl: 'new-tmpl'
+            })
+            .when('/sheet/:id', {
+                templateUrl: 'sheet-tmpl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }]);
+
